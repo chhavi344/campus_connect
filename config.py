eb0cc2ae1149dee5b5d0cb31259dbb648a6ca560
chhavi <chhavibalmik72@GMAIL.COM>
2026-08-01 10:23:44 +0530

    file create

--- config.py
+++ config.py
@@ -2,5 +2,5 @@
 DB_HOST = "mysql-2ff42440-chhavibalmik72-9db3.e.aivencloud.com"
 DB_USER = "avnadmin"
 DB_PASSWORD = "AVNS_2mf2P0@T0n_zJbKV59i"
-DB_NAME = "defaultdb"
+DB_NAME = "campusconnect"
 DB_PORT = 11604
